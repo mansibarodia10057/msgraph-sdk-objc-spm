9f5bbe9fa29420ff51f54dd2c8a294e7b04424bc
Mansi Barodia <49169550+mansibarodia10057@users.noreply.github.com>
2024-10-01 21:55:33 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,18 +4,17 @@
 import PackageDescription
 
 let package = Package(
-    name: "msgraph-sdk-objc-spm",
+    name: "MSGraphClientSDK",
+    platforms: [
+        .iOS(.v16)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "msgraph-sdk-objc-spm",
-            targets: ["msgraph-sdk-objc-spm"]),
+            name: "MSGraphClientSDK",
+            targets: ["MSGraphClientSDK"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "msgraph-sdk-objc-spm"),
-
+        .binaryTarget(name: "MSGraphClientSDK", url: "https://github.com/mansibarodia10057/msgraph-sdk-objc/releases/download/1.0.0-spm/MSGraphClientSDK.xcframework.zip", checksum: "99ff4efa935130ee9ee64c31fc2846498e7dee920872815bd41f5204fec06bba")
     ]
 )
